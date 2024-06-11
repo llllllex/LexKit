c3c950b0f73afe6dc586a929cc268de48046e1d6
Lex <kfc56@live.cn>
2024-06-11 16:01:09 +0800

    MOD: modify Package.swift

--- Package.swift
+++ Package.swift
@@ -5,21 +5,39 @@ import PackageDescription
 
 let package = Package(
     name: "LexKit",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v17),
+        .tvOS(.v17),
+        .watchOS(.v10),
+        .macCatalyst(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LexKit",
-            targets: ["LexKit"]),
+            targets: [
+                "LexKit"
+            ]
+        ),
+    ],
+    dependencies:[
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LexKit"),
+            name: "LexKit",
+            dependencies: [
+            
+            ]
+        ),
         .testTarget(
             name: "LexKitTests",
-            dependencies: ["LexKit"]
+            dependencies: [
+                "LexKit"
+            ]
         ),
     ]
 )
